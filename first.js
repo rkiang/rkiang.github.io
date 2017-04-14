11615b8e0720b65a59f4dd19d8152f0224117d3b
rkiang <ryanlkiang@gmail.com>
2017-04-14 15:33:42 -0500

    reverted to alert

--- first.js
+++ first.js
@@ -1,11 +1,1 @@
-function films2() {
-  $('.features').hide();
-  $('.features').fadeIn(9000);
-  $('.description').hide();
-  $('.titles').on('click', function().
-  {
-$(this).next().slideToggle(4000);
-$(this).toggleClass('active');
-  });
-}
-$(document).ready(films2);
+alert('working');
