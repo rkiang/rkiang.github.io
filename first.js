2b1ff7d79282aab93822fff3c1231680af161b6e
rkiang <ryanlkiang@gmail.com>
2017-04-14 15:16:20 -0500

    added fade in

--- first.js
+++ first.js
@@ -1,1 +1,11 @@
-alert('working');
+function films2() {
+  $('.features').hide();
+  $('.features').fadeIn(9000);
+  $('.description').hide();
+  $('.titles').on('click', function().
+  {
+$(this).next().slideToggle(4000);
+$(this).toggleClass('active');
+  });
+}
+$(document).ready(films2);
